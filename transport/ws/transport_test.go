9a31fd835e8c2171e430f6aa35ed14c335dd17d1
Timo Friedl <timo@babelforce.com>
2025-06-25 08:39:46 +0200

    fix(ws): fixed test

--- transport/ws/transport_test.go
+++ transport/ws/transport_test.go
@@ -22,14 +22,14 @@ func TestClientServer(t *testing.T) {
 	require.NoError(t, srv.Run(ctx))
 
 	// Connect client transport
-	clientTransport, err := Connect(ctx, ClientConfig{Dial: DialConfig{URL: fmt.Sprintf("ws://127.0.0.1:%d", srv.Port())}})
-	require.NoError(t, err)
-
-	client := rtvbp.NewSession(clientTransport, rtvbp.SessionConfig{}, rtvbp.NewHandler(rtvbp.HandlerConfig{}))
-	go client.Run(ctx)
+	sess := rtvbp.NewSession(
+		Client(ClientConfig{Dial: DialConfig{URL: fmt.Sprintf("ws://127.0.0.1:%d", srv.Port())}}),
+		rtvbp.NewHandler(rtvbp.HandlerConfig{}),
+	)
+	go sess.Run(ctx)
 	<-time.After(1 * time.Second)
-	require.NoError(t, client.CloseTimeout(5*time.Second))
-	require.NoError(t, client.CloseTimeout(5*time.Second))
+	require.NoError(t, sess.CloseTimeout(5*time.Second))
+	require.NoError(t, sess.CloseTimeout(5*time.Second))
 
 	// CloseTimeout transport
 	closeCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
